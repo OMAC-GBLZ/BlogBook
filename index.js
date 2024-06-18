783c215a0654143c149771c19bb102f45425eb4a
OMAC-GBLZ <Odhran@DESKTOP-0TKIJFB>
2024-06-18 14:55:38 +0100

    functionality implemented

--- index.js
+++ index.js
@@ -6,10 +6,49 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
-    res.render("index.ejs");
-});
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+  });
+  
+  var postsCollection = [];
+    
+  app.post("/submit", (req, res)=> {
+    var post = new BlogPost(req.body["name"],req.body["title"],req.body["content"],postsCollection.length + 1, new Date())
+    postsCollection.push(post);
+    res.render("index.ejs", {
+        createdPosts: postsCollection,
+    });
+  });
+
+  app.post("/delete", (req, res)=> {
+    console.log(req.body["postId"] + ": deleted");
+    let index = Number(Number(req.body["postId"]) - 1);
+    console.log(index);
+    postsCollection.splice(index, 1);
+    res.render("index.ejs", {
+        createdPosts: postsCollection,
+    });
+  });
+
+  app.post("/update", (req, res)=>{
+    let index = Number(Number(req.body["postId"]) - 1);
+    postsCollection[index].title = req.body["title"];
+    postsCollection[index].body = req.body["content"];
+    console.log(postsCollection[index].user);
+    res.render("index.ejs", {
+        createdPosts: postsCollection,
+    });
+  });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+
+  function BlogPost(user, title, body, id, time){
+    this.user = user;
+    this.title = title;
+    this.body = body;
+    this.id = id;
+    this.time = time;
+  }
